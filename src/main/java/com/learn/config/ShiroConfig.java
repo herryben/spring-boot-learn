cf5222aa70e8f5d83ef65caaee26d9037df1a5e4
herry <397283864@qq.com>
2018-08-04 16:10:36 +0800

    add shiro cache and session

--- src/main/java/com/learn/config/ShiroConfig.java
+++ src/main/java/com/learn/config/ShiroConfig.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Maps;
 import com.learn.auth.CustomRealm;
 import com.learn.bean.SystemPermission;
 import com.learn.biz.SystemPermissionBiz;
+import org.apache.shiro.mgt.SecurityManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
@@ -12,10 +13,8 @@ import org.crazycake.shiro.RedisManager;
 import org.crazycake.shiro.RedisSessionDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.apache.shiro.mgt.SecurityManager;
 
 import java.util.List;
 import java.util.Map;
