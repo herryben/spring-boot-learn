a89545e9e227d995a297e78364b1a83dfca9d96c
herry <397283864@qq.com>
2018-08-11 16:32:23 +0800

    add Lists.partition()

--- src/test/java/com/learn/LearnSpringbootApplicationTests.java
+++ src/test/java/com/learn/LearnSpringbootApplicationTests.java
@@ -1,5 +1,6 @@
 package com.learn;
 
+import com.google.common.collect.Lists;
 import org.springframework.context.ApplicationContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -20,5 +22,16 @@ public class LearnSpringbootApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
+        List<Integer> list = Lists.newArrayList();
+        for (int i = 0; i < 54; i++) {
+            list.add(i);
+        }
+        List<List<Integer>> cache = Lists.partition(list, 10);
+        for (List<Integer> tmpList: cache) {
+            for (final Integer i: tmpList) {
+                LOGGER.info("this is {}", i);
+            }
+            LOGGER.info("===============");
+        }
+    }
 }
