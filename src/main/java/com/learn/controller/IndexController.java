6d13cfa69928efde0bc8cf2aefb32289c41f5c18
herry <397283864@qq.com>
2018-08-12 19:38:09 +0800

    cache request

--- src/main/java/com/learn/controller/IndexController.java
+++ src/main/java/com/learn/controller/IndexController.java
@@ -3,7 +3,9 @@ package com.learn.controller;
 import com.learn.bean.AuthorSettings;
 import com.learn.bean.HttpResponse;
 import com.learn.bean.User;
+import com.learn.command.GetUserServiceCommand;
 import com.learn.service.UserService;
+import com.netflix.hystrix.strategy.concurrency.HystrixRequestContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -45,5 +47,18 @@ public class IndexController {
     @GetMapping("users")
     public HttpResponse getAllUsers() {
         return new HttpResponse(userService.getAllUsers());
+    }
+
+    @GetMapping("hystrix")
+    public HttpResponse getHystrix() {
+        HystrixRequestContext context = HystrixRequestContext.initializeContext();
+        GetUserServiceCommand command1 = new GetUserServiceCommand(userService, 1);
+        GetUserServiceCommand command2 = new GetUserServiceCommand(userService, 1);
+        GetUserServiceCommand command3 = new GetUserServiceCommand(userService, 1);
+        command1.execute();
+        command2.execute();
+        command3.execute();
+        context.shutdown();
+        return new HttpResponse();
     }
 }
