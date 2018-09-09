ab0c63c1b70a877065a576787cdc7ab2352730d1
herry <397283864@qq.com>
2018-09-09 19:45:53 +0800

    add test sort

---
+++ src/test/java/com/learn/TestSort.java
@@ -0,0 +1,78 @@
+package com.learn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestSort {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestSort.class);
+    @Test
+    public void testSelect() {
+        int array[] = new int[10];
+        int tmp;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int)(1+Math.random()*100);
+        }
+        LOGGER.info("raw data {} ", array);
+        LOGGER.info("====================");
+        for (int i = 0; i < array.length; i++) {
+            int k = i;
+            for (int j = array.length - 1; j >  i; j--) {
+                if (array[k] < array[j]) {
+                    k = j;
+                }
+            }
+            if (k != i) {
+                tmp = array[i];
+                array[i] = array[k];
+                array[k] = tmp;
+            }
+        }
+        LOGGER.info("sorted data {} ", array);
+    }
+
+    @Test
+    public void testBubble() {
+        int array[] = new int[10];
+        int tmp;
+        for (int i = 0; i < array.length - 1; i++) {
+            array[i] = (int)(1+Math.random()*100);
+        }
+        LOGGER.info("raw data {} ", array);
+        LOGGER.info("====================");
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    tmp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = tmp;
+                }
+            }
+        }
+        LOGGER.info("sorted data {} ", array);
+    }
+
+    @Test
+    public void testInsert() {
+        int array[] = new int[10];
+        int tmp, j;
+        for (int i = 0; i < array.length - 1; i++) {
+            array[i] = (int)(1+Math.random()*100);
+        }
+        LOGGER.info("raw data {} ", array);
+        LOGGER.info("====================");
+        for (int i = 0; i < array.length ; i++) {
+            tmp = array[i];
+            for (j = i; j > 0 && tmp > array[j-1] ; j++) {
+                array[j] = array[j - 1];
+            }
+            array[j] = tmp;
+        }
+        LOGGER.info("sorted data {} ", array);
+    }
+}
