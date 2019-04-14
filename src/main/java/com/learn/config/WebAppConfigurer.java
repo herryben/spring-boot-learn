7fbd14656d530b3b42eed6afdce737906522eee0
haorui <397283864@qq.com>
2019-04-14 17:35:12 +0800

    add WebAppConfigurer

---
+++ src/main/java/com/learn/config/WebAppConfigurer.java
@@ -0,0 +1,21 @@
+package com.learn.config;
+
+import com.learn.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfigurer implements WebMvcConfigurer{
+
+    @Bean
+    public LoginInterceptor loginInterceptor(){
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor()).addPathPatterns("/**");
+    }
+}
