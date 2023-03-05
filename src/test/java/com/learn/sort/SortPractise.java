f447dbffff7ad9473574abc039bdd178e7a85ad0
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-03-05 12:09:40 +0800

    feat:增加联系模板SortPractise

--- src/test/java/com/learn/sort/SortPractise.java
+++ src/test/java/com/learn/sort/SortPractise.java
@@ -1,21 +1,60 @@
 package com.learn.sort;
 
+import org.junit.Test;
+
 /**
  * @author herryhaorui@didiglobal.com
  * @desc
  * @date 2023/3/3 10:14 下午
  */
 public class SortPractise {
-    private int N = 10;
-    private int[] datas = new int[N];
-    public void init(){
+    private final int N = 10;
+    private final int[] datas = new int[N];
+
+    public void init() {
         for (int i = 0; i < datas.length; i++) {
-            datas[i] = (int)(Math.random() * 100 + 1);
+            datas[i] = (int) (Math.random() * 100 + 1);
         }
     }
 
-    public void print(){
-        for (int data: datas) {
+    @Test
+    public void testBubble() {
+        init();
+        print();
+        print();
+    }
+
+    @Test
+    public void testSelect() {
+        init();
+        print();
+        print();
+    }
+
+    @Test
+    public void testInsert() {
+        init();
+        print();
+        print();
+    }
+
+
+    @Test
+    public void testShell() {
+        init();
+        print();
+        print();
+    }
+
+    @Test
+    public void testQsort() {
+        init();
+        print();
+        print();
+    }
+
+    public void print() {
+        for (int data : datas) {
             System.out.print(data + " ");
         }
         System.out.println();
--- src/test/java/com/learn/sort/TestSort.java
+++ src/test/java/com/learn/sort/TestSort.java
@@ -8,19 +8,22 @@ import org.slf4j.LoggerFactory;
 //@SpringBootTest
 public class TestSort {
     private static final Logger LOGGER = LoggerFactory.getLogger(TestSort.class);
+    private final int N = 5;
+    private final int[] datas = new int[N];
+
     @Test
     public void testSelect() {
-        int array[] = new int[10];
+        int[] array = new int[10];
         int tmp;
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int)(1+Math.random()*100);
+            array[i] = (int) (1 + Math.random() * 100);
         }
         LOGGER.info("raw data {} ", array);
         LOGGER.info("====================");
         for (int i = 0; i < array.length; i++) {
             int k = i;
-            for (int j = array.length - 1; j >  i; j--) {
-                if (array[k] < array[j]) {
+            for (int j = array.length - 1; j > i; j--) {
+                if (array[k] > array[j]) {
                     k = j;
                 }
             }
@@ -35,7 +38,7 @@ public class TestSort {
 
     @Test
     public void testBubble() {
-        int array[] = new int[10];
+        int[] array = new int[10];
         int tmp;
         for (int i = 0; i < array.length - 1; i++) {
             array[i] = (int)(1+Math.random()*100);
@@ -56,7 +59,7 @@ public class TestSort {
 
     @Test
     public void testInsert() {
-        int array[] = new int[10];
+        int[] array = new int[10];
         int tmp, j;
         for (int i = 0; i < array.length - 1; i++) {
             array[i] = (int)(1+Math.random()*100);
@@ -73,21 +76,29 @@ public class TestSort {
         for (int i = 1; i < array.length; i++) {
             if (array[i] < array[i - 1]) {
                 tmp = array[i];
-                for (j = i-1; j>=0 && tmp < array[j]; j--) {//j--会执行到-1
-                    array[j +1] = array[j];
+                for (j = i - 1; j >= 0 && tmp < array[j]; j--) {//j--会执行到-1
+                    array[j + 1] = array[j];
                 }
-                array[j+1] = tmp;
+                array[j + 1] = tmp;
             }
         }
         LOGGER.info("sorted data {} ", array);
     }
 
+    public void qsort(int[] data, int left, int right) {
+        if (left < right) {
+            int idx = partition(data, left, right);
+            qsort(data, left, idx - 1);
+            qsort(data, idx + 1, right);
+        }
+    }
+
     @Test
     public void testShell() {
-        int array[] = new int[10];
+        int[] array = new int[10];
         int j;
         for (int i = 0; i < array.length - 1; i++) {
-            array[i] = (int)(1+Math.random()*100);
+            array[i] = (int) (1 + Math.random() * 100);
         }
         LOGGER.info("raw data {} ", array);
         for (int gap = array.length / 2; gap != 0; gap = gap / 2) {
@@ -108,10 +119,10 @@ public class TestSort {
      */
     @Test
     public void testQSort() {
-        int array[] = new int[10];
+        int[] array = new int[10];
         int tmp, j;
         for (int i = 0; i < array.length - 1; i++) {
-            array[i] = (int)(1+Math.random()*100);
+            array[i] = (int) (1 + Math.random() * 100);
         }
         LOGGER.info("raw data {} ", array);
         LOGGER.info("====================");
@@ -119,20 +130,29 @@ public class TestSort {
         LOGGER.info("sorted data {} ", array);
     }
 
-    public void qsort(int[] data, int left, int right) {
-        if (left < right) {
-            int idx = partition(data, left, right);
-            qsort(data, left, idx - 1);
-            qsort(data, idx + 1, right);
+    public int partition(int[] data, int low, int high) {
+        int pivot = data[low];
+        int i = low, j = high;
+        while (i < j) {
+            while (i < j && data[j] >= pivot) {
+                --j;
+            }
+            data[i] = data[j];
+            while (i < j && data[i] < pivot) {
+                ++i;
+            }
+            data[j] = data[i];
         }
+        data[i] = pivot;
+        return i;
     }
 
     @Test
     public void testQsortTwentyLines() {
-        int array[] = new int[5];
+        int[] array = new int[5];
         int tmp, j;
         for (int i = 0; i < array.length - 1; i++) {
-            array[i] = (int)(1+Math.random()*100);
+            array[i] = (int) (1 + Math.random() * 100);
         }
         LOGGER.info("raw data {} ", array);
         LOGGER.info("====================");
@@ -161,32 +181,12 @@ public class TestSort {
             while (i < j && data[i] < pivot) {
                 i++;
             }
-            data[j] = data[i];;
+            data[j] = data[i];
         }
         data[i] = pivot;
         qsortTwentyLines(data, low, i - 1);
         qsortTwentyLines(data, i + 1, high);
     }
-
-    public int partition(int[] data, int low, int high) {
-        int pivot = data[low];
-        int i = low, j = high;
-        while (i < j) {
-            while (i < j && data[j] >= pivot) {
-                --j;
-            }
-            data[i] = data[j];
-            while (i < j && data[i] < pivot) {
-                ++i;
-            }
-            data[j] = data[i];
-        }
-        data[i] = pivot;
-        return i;
-    }
-
-    private int N = 5;
-    private int[] datas = new int[N];
     public void init(){
         for (int i = 0; i < datas.length; i++) {
             datas[i] = (int)(Math.random() * 100 + 1);
