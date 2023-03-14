618045e0fad41df5ff3f498d4e781ffc5d66c1d0
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-03-14 22:00:59 +0800

    feat:增加 300. 最长递增子序列

--- src/test/java/com/learn/dp/DpSolution.java
+++ src/test/java/com/learn/dp/DpSolution.java
@@ -198,4 +198,47 @@ public class DpSolution {
         Assert.assertEquals(coinChangeOfficial(new int[]{2}, 3), -1);
         Assert.assertEquals(coinChangeOfficial(new int[]{1}, 0), 0);
     }
+
+    /**
+     * 300. 最长递增子序列
+     * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+     * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+     * 示例 1：
+     * 输入：nums = [10,9,2,5,3,7,101,18]
+     * 输出：4
+     * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+     * 示例 2：
+     * 输入：nums = [0,1,0,3,2,3]
+     * 输出：4
+     * 示例 3：
+     * 输入：nums = [7,7,7,7,7,7,7]
+     * 输出：1
+     * 解题思路：
+     * 定义 dp[i]为考虑前 i 个元素，以第 i 个数字结尾的最长上升子序列的长度，注意 nums[i] 必须被选取。
+     *
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        // 初始状态下每个长度肯定包含自己
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                // 当前元素比之前的元素大才更新
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        // 返回最大的那个
+        return Arrays.stream(dp).max().getAsInt();
+    }
+
+    @Test
+    public void testLengthOfLIS() {
+        Assert.assertEquals(4, lengthOfLIS(new int[]{10, 9, 2, 5, 3, 7, 101, 18}));
+        Assert.assertEquals(4, lengthOfLIS(new int[]{0, 1, 0, 3, 2, 3}));
+        Assert.assertEquals(1, lengthOfLIS(new int[]{7, 7, 7, 7, 7, 7, 7}));
+    }
 }
