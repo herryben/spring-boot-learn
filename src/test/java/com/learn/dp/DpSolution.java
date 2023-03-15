667e6e78568d2f3b3e43e08f8dda8767c731cf2a
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-03-15 22:59:08 +0800

    feat:增加 0/1背包

--- src/test/java/com/learn/dp/DpSolution.java
+++ src/test/java/com/learn/dp/DpSolution.java
@@ -277,4 +277,35 @@ public class DpSolution {
         }
         return low;
     }
+
+    /**
+     * 01背包
+     * 给你一个可装载重量为 W 的背包和 N 个物品，每个物品有重量和价值两 个属性。其中第 i 个物品的重量为 wt[i] ，
+     * 价值为 val[i] ，现在让你用 这个背包装物品，最多能装的价值是多少?
+     * N = 3, W = 4
+     * wt = [2, 1, 3]
+     * val = [4, 2, 3]
+     * 算法返回6
+     * 解题思路：dp[i][j] i件物品重量为j时的最大价值
+     */
+    public int knapsack(int W, int N, int[] wt, int[] val) {
+        int[][] dp = new int[N + 1][W + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int w = 1; w <= W; w++) {
+                if (w - wt[i - 1] < 0) {
+                    // 背包剩余重量不够 这种情况下只能选择不装入背包
+                    dp[i][w] = dp[i - 1][w];
+                } else {
+                    // 不装入和装入，两者之间选最大值
+                    dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - wt[i - 1]] + val[i - 1]);
+                }
+            }
+        }
+        return dp[N][W];
+    }
+
+    @Test
+    public void testKnapsack() {
+        Assert.assertEquals(6, knapsack(4, 3, new int[]{2, 1, 3}, new int[]{4, 2, 3}));
+    }
 }
