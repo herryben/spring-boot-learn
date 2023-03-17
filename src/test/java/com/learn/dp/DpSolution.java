4719ec6b6dec798a6f6deca41afd86dc2a9ecbd2
haorui <herryhaorui@didiglobal.com>
2023-03-17 21:59:03 +0800

    feat： 416.分割等和子集

--- src/test/java/com/learn/dp/DpSolution.java
+++ src/test/java/com/learn/dp/DpSolution.java
@@ -345,4 +345,50 @@ public class DpSolution {
         Assert.assertEquals(4, completeNapsack(5, new int[]{1, 2, 5}));
         Assert.assertEquals(0, completeNapsack(3, new int[]{2}));
     }
+
+    /**
+     * 416.分割等和子集
+     * 给定一个只包含正整数的非空数组，是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+     * 注意：
+     * 1.每个数组中的元素不会超过100
+     * 2.数组的大小不会超过200
+     * 示例1：
+     *  输入：[1, 5, 11, 5]
+     *  输出：true
+     *  解释：数组可以分割成[1, 5, 5] 和 [11]
+     * 示例2：
+     *  输入：[1, 2, 3, 5]
+     *  输出：false
+     *  解释：数组不能分割成两个元素和相等的子集
+     * 解题思路：
+     *  用 num / 2 转化成0/1背包思路
+     */
+    public boolean canPartition (int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        int n = sum % 2;
+        if (n == 1) {
+            return false;
+        }
+        boolean[][] dp = new boolean[nums.length+1][n + 1];
+        for (int i = 0; i <= nums.length; i++) {
+            dp[i][0] = true;
+        }
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j >= nums[i-1]) {
+                    dp[i][j] = dp[i-1][j] || dp[i-1][j-nums[i-1]];
+                } else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[nums.length][n];
+    }
+
+    @Test
+    public void testCanPartition() {
+        Assert.assertEquals(true, canPartition(new int[] {1,5,11,5}));
+        Assert.assertEquals(false, canPartition(new int[] {1,2,3,5}));
+    }
+
 }
