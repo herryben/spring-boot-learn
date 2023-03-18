677703e2c7957f40ccaacc6b77dc9d8c34dc8479
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-03-18 17:50:40 +0800

    feat:增加 2389. 和有限的最长子序列

--- src/test/java/com/learn/sort/SortSolution.java
+++ src/test/java/com/learn/sort/SortSolution.java
@@ -136,6 +136,7 @@ public class SortSolution {
 
     /**
      * 2389. 和有限的最长子序列
+     * https://leetcode.cn/problems/longest-subsequence-with-limited-sum/description/
      * 给你一个长度为 n 的整数数组 nums ，和一个长度为 m 的整数数组 queries 。
      * 返回一个长度为 m 的数组 answer ，其中 answer[i] 是 nums 中 元素之和小于等于 queries[i] 的 子序列 的 最大 长度  。
      * 子序列 是由一个数组删除某些元素（也可以不删除）但不改变剩余元素顺序得到的一个数组。
@@ -151,7 +152,6 @@ public class SortSolution {
      * 输出：[0]
      * 解释：空子序列是唯一一个满足元素和小于或等于 1 的子序列，所以 answer[0] = 0 。
      * 解题思路：前缀和加二分搜索
-     *
      * @param nums
      * @param queries
      * @return
