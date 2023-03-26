5460a6cf1e3665a01f1e0be2e7ffd53f6e0e56a0
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-03-26 21:16:52 +0800

    feat:增加 215. 数组中的第K个最大元素

--- src/test/java/com/learn/sort/SortSolution.java
+++ src/test/java/com/learn/sort/SortSolution.java
@@ -190,4 +190,31 @@ public class SortSolution {
         Assert.assertEquals(true, Arrays.equals(new int[]{2, 3, 4}, answerQueries(new int[]{4, 5, 2, 1}, new int[]{3, 10, 21})));
         Assert.assertEquals(true, Arrays.equals(new int[]{0}, answerQueries(new int[]{2, 3, 4, 5}, new int[]{1})));
     }
+
+    /**
+     * 215. 数组中的第K个最大元素
+     * https://leetcode.cn/problems/kth-largest-element-in-an-array/description/
+     * 示例 1:
+     * 输入: [3,2,1,5,6,4], k = 2
+     * 输出: 5
+     * 示例 2:
+     * 输入: [3,2,3,1,2,4,5,5,6], k = 4
+     * 输出: 4
+     * 给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。
+     * 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
+     * 你必须设计并实现时间复杂度为 O(n) 的算法解决此问题。
+     *
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int findKthLargest(int[] nums, int k) {
+        return nums[k];
+    }
+
+    @Test
+    public void testFindKthLargest() {
+        Assert.assertEquals(5, findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2));
+        Assert.assertEquals(4, findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4));
+    }
 }
