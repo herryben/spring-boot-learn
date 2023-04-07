4f9defb02d561d29085ee939051a8072dd1c6357
haorui <herryhaorui@didiglobal.com>
2023-04-07 18:40:35 +0800

    feat： 739. 每日温度

--- src/test/java/com/learn/stack/StackSolution.java
+++ src/test/java/com/learn/stack/StackSolution.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class StackSolution {
     /**
-     * TODO 739. 每日温度
+     * 739. 每日温度
      * https://leetcode.cn/problems/daily-temperatures/
      * 给定一个整数数组 temperatures ，表示每天的温度，返回一个数组 answer ，其中 answer[i] 是指对于第 i 天，下一个更高温度出现在几天后。
      * 如果气温在这之后都不会升高，请在该位置用 0 来代替。
@@ -25,7 +25,16 @@ public class StackSolution {
      * @return
      */
     public int[] dailyTemperatures(int[] temperatures) {
-        return temperatures;
+        Deque<Integer> stack = new LinkedList<>();
+        int[] ans = new int[temperatures.length];
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && temperatures[i] >= temperatures[stack.peekFirst()]) {
+                stack.pop();
+            }
+            ans[i] = stack.isEmpty() ? 0 : stack.peekFirst() - i;
+            stack.push(i);
+        }
+        return ans;
     }
 
     @Test
