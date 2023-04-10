287cc1d125e07047a757d4acf3449ebf917ba816
haorui <herryhaorui@didiglobal.com>
2023-04-10 17:16:14 +0800

    feat： 42. 接雨水 dp法

--- src/test/java/com/learn/stack/StackSolution.java
+++ src/test/java/com/learn/stack/StackSolution.java
@@ -371,4 +371,31 @@ public class StackSolution {
         Assert.assertEquals(6, trap(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
         Assert.assertEquals(9, trap(new int[]{4, 2, 0, 3, 2, 5}));
     }
+
+    public int trapDp(int[] height) {
+        int res = 0;
+        int[] leftMax = new int[height.length];
+        int[] rightMax = new int[height.length];
+        leftMax[0] = height[0];
+        for (int i = 1; i < height.length; i++) {
+            leftMax[i] = Math.max(height[i], leftMax[i - 1]);
+        }
+        rightMax[height.length - 1] = height[height.length - 1];
+        for (int i = height.length - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(height[i], rightMax[i + 1]);
+        }
+        for (int i = 0; i < height.length; i++) {
+            int tmp = Math.min(leftMax[i], rightMax[i]);
+            if (tmp > height[i]) {
+                res += tmp - height[i];
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void testTrapDp() {
+        Assert.assertEquals(6, trapDp(new int[]{0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1}));
+        Assert.assertEquals(9, trapDp(new int[]{4, 2, 0, 3, 2, 5}));
+    }
 }
