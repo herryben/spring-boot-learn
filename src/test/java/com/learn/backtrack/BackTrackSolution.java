d9bb837e728f909242fa918db16359c37c65e5cf
haorui <herryhaorui@didiglobal.com>
2023-07-07 09:41:48 +0800

    feat： 39. 组合总和

--- src/test/java/com/learn/backtrack/BackTrackSolution.java
+++ src/test/java/com/learn/backtrack/BackTrackSolution.java
@@ -268,4 +268,79 @@ public class BackTrackSolution {
         Assert.assertEquals(true, SetUtils.isEqualSet(Sets.newHashSet("((()))","(()())","(())()","()(())","()()()"), Sets.newHashSet(generateParenthesis(3))));
         Assert.assertEquals(true, SetUtils.isEqualSet(Sets.newHashSet("()"), Sets.newHashSet(generateParenthesis(1))));
     }
+
+    /**
+     * 39. 组合总和
+     * https://leetcode.cn/problems/combination-sum/description/
+     * 给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+     *
+     * candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+     *
+     * 对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+     * 示例 1：
+     *
+     * 输入：candidates = [2,3,6,7], target = 7
+     * 输出：[[2,2,3],[7]]
+     * 解释：
+     * 2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+     * 7 也是一个候选， 7 = 7 。
+     * 仅有这两种组合。
+     * 示例 2：
+     *
+     * 输入: candidates = [2,3,5], target = 8
+     * 输出: [[2,2,2,2],[2,3,3],[3,5]]
+     * 示例 3：
+     *
+     * 输入: candidates = [2], target = 1
+     * 输出: []
+     * 解题思路：组合的求和变种
+     * @param candidates
+     * @param target
+     * @return
+     */
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        combinationSum(candidates, 0, 0, target, result, new LinkedList<>());
+//        dfs(result, candidates, 0, new LinkedList<>(), target, 0);
+        return result;
+    }
+
+    public void combinationSum(int[] candidates, int sum, int start, int target, List<List<Integer>> result, List<Integer> track) {
+        if (sum == target) {
+            result.add(new ArrayList<>(track));
+            return;
+        }
+        for (int i = start; i < candidates.length; i++) {
+            if(sum + candidates[i] > target) {
+                return;
+            }
+            track.add(candidates[i]);
+            combinationSum(candidates, sum + candidates[i], i, target, result, track);
+            track.remove(track.size() - 1);
+        }
+    }
+
+    public void dfs(List<List<Integer>> res, int[] candidates, int tmp, List<Integer> path, int target, int start){
+        if(tmp == target){
+            res.add(path);
+            return;
+        }
+        for(int i =start ;i < candidates.length; i++){
+            if(tmp + candidates[i] > target){
+                return;
+            }
+            path.add(candidates[i]);
+            dfs(res, candidates, tmp + candidates[i], new ArrayList<>(path), target, i);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    @Test
+    public void testCombination() {
+        Assert.assertEquals(true, SetUtils.isEqualSet(Sets.newHashSet(
+                Lists.newArrayList(2,2,3), Lists.newArrayList(7)), Sets.newHashSet(combinationSum(new int[] {2,3,6,7}, 7))));
+        Assert.assertEquals(true, SetUtils.isEqualSet(Sets.newHashSet(
+                Lists.newArrayList(2, 2, 2, 2), Lists.newArrayList(2, 3, 3), Lists.newArrayList(3, 5)), Sets.newHashSet(combinationSum(new int[] {2,3,5}, 8))));
+        Assert.assertEquals(true, SetUtils.isEqualSet(Sets.newHashSet(), Sets.newHashSet(combinationSum(new int[] {2}, 1))));
+    }
 }
