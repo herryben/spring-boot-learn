ca981a9de8bf96e018f1b0d796cccdc8cf8f6959
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-09-02 15:22:27 +0800

    feat：337. 打家劫舍 III

--- src/test/java/com/learn/tree/TreeSolution.java
+++ src/test/java/com/learn/tree/TreeSolution.java
@@ -448,4 +448,36 @@ public class TreeSolution {
         Assert.assertEquals(true, Utils.isBinaryTreeArrayEqual(invertTree(Utils.buildBinaryTree(new Integer[]{2, 1, 3})), new Integer[]{2, 3, 1}));
     }
 
+    /**
+     * 337. 打家劫舍 III
+     * https://leetcode.cn/problems/house-robber-iii/description/
+     * 小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为 root 。
+     * 除了 root 之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。 如果 两个直接相连的房子在同一天晚上被打劫 ，房屋将自动报警。
+     * 给定二叉树的 root 。返回 在不触动警报的情况下 ，小偷能够盗取的最高金额 。
+     *
+     * @param root
+     * @return
+     */
+    public int rob(TreeNode root) {
+        int[] ans = search(root);
+        return Math.max(ans[0], ans[1]);
+    }
+
+    public int[] search(TreeNode root) {
+        int[] rob = new int[2];
+        if (root == null) {
+            return rob;
+        }
+        int[] left = search(root.left);
+        int[] right = search(root.right);
+        rob[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+        rob[1] = root.val + left[0] + right[0];
+        return rob;
+    }
+
+    @Test
+    public void testRob() {
+        Assert.assertEquals(7, rob(Utils.buildBinaryTree(new Integer[]{3, 2, 3, null, 3, null, 1})));
+        Assert.assertEquals(9, rob(Utils.buildBinaryTree(new Integer[]{3, 4, 5, 1, 3, null, 1})));
+    }
 }
