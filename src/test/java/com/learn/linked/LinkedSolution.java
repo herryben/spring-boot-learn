dd71d7fd818455a1f6d52d29e23c6530bcd694c4
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-09-24 15:02:54 +0800

    feat：138. 随机链表的复制

--- src/test/java/com/learn/linked/LinkedSolution.java
+++ src/test/java/com/learn/linked/LinkedSolution.java
@@ -4,9 +4,7 @@ import com.learn.Utils.Utils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * @author herryhaorui@didiglobal.com
@@ -1036,5 +1034,55 @@ public class LinkedSolution {
     public void testReverseBetween() {
         Assert.assertTrue(Utils.isLinkedListArrayEqual(reverseBetween(Utils.buildLinkedList(new int[]{1, 2, 3, 4, 5}), 2, 4), new int[]{1, 4, 3, 2, 5}));
         Assert.assertTrue(Utils.isLinkedListArrayEqual(reverseBetween(Utils.buildLinkedList(new int[]{5}), 1, 1), new int[]{5}));
+    }
+
+    /**
+     * 138. 随机链表的复制
+     * https://leetcode.cn/problems/copy-list-with-random-pointer/description/?envType=study-plan-v2&envId=top-100-liked
+     * 给你一个长度为 n 的链表，每个节点包含一个额外增加的随机指针 random ，该指针可以指向链表中的任何节点或空节点。
+     * <p>
+     * 构造这个链表的 深拷贝。 深拷贝应该正好由 n 个 全新 节点组成，其中每个新节点的值都设为其对应的原节点的值。新节点的 next 指针和 random 指针也都应指向复制链表中的新节点，并使原链表和复制链表中的这些指针能够表示相同的链表状态。复制链表中的指针都不应指向原链表中的节点 。
+     * <p>
+     * 例如，如果原链表中有 X 和 Y 两个节点，其中 X.random --> Y 。那么在复制链表中对应的两个节点 x 和 y ，同样有 x.random --> y 。
+     * <p>
+     * 返回复制链表的头节点。
+     * <p>
+     * 用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+     * <p>
+     * val：一个表示 Node.val 的整数。
+     * random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+     * 你的代码 只 接受原链表的头节点 head 作为传入参数。
+     * 示例 1：
+     * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+     * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+     * 示例 2：
+     * 输入：head = [[1,1],[2,1]]
+     * 输出：[[1,1],[2,1]]
+     * 示例 3：
+     * 输入：head = [[3,null],[3,0],[3,null]]
+     * 输出：[[3,null],[3,0],[3,null]]
+     * 解题思路：
+     * 1. 直接先把所有的都复制出来
+     * 2. 构造新链表
+     *
+     * @param head
+     * @return
+     */
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return head;
+        }
+        Map<Node, Node> map = new HashMap<>();
+
+        for (Node cur = head; cur != null; cur = cur.next) {
+            map.put(cur, new Node(cur.val));
+        }
+
+        for (Node cur = head; cur != null; cur = cur.next) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+        }
+
+        return map.get(head);
     }
 }
