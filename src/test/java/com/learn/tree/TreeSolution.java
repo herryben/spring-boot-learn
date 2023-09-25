544fb40ab45801bd479c64955020fe49d68f0de2
zenghaonan_i <zenghaonan_i@didiglobal.com>
2023-09-25 11:20:22 +0800

    feat：437. 路径总和 III

--- src/test/java/com/learn/tree/TreeSolution.java
+++ src/test/java/com/learn/tree/TreeSolution.java
@@ -534,4 +534,56 @@ public class TreeSolution {
         Assert.assertEquals(7, rob(Utils.buildBinaryTree(new Integer[]{3, 2, 3, null, 3, null, 1})));
         Assert.assertEquals(9, rob(Utils.buildBinaryTree(new Integer[]{3, 4, 5, 1, 3, null, 1})));
     }
+
+    /**
+     * 437. 路径总和 III
+     * 给定一个二叉树的根节点 root ，和一个整数 targetSum ，求该二叉树里节点值之和等于 targetSum 的 路径 的数目。
+     * <p>
+     * 路径 不需要从根节点开始，也不需要在叶子节点结束，但是路径方向必须是向下的（只能从父节点到子节点）。
+     * 示例 1：
+     * 输入：root = [10,5,-3,3,2,null,11,3,-2,null,1], targetSum = 8
+     * 输出：3
+     * 解释：和等于 8 的路径有 3 条，如图所示。
+     * 示例 2：
+     * <p>
+     * 输入：root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+     * 输出：3
+     * 解题思路
+     * dfs通用模板 + 双递归
+     * https://leetcode.cn/problems/path-sum-iii/solutions/815688/yi-pian-wen-zhang-jie-jue-suo-you-er-cha-smch/?envType=study-plan-v2&envId=top-100-liked
+     *
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public int pathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+        int ans = dfs(root, targetSum);
+        ans += pathSum(root.left, targetSum);
+        ans += pathSum(root.right, targetSum);
+        return ans;
+    }
+
+    public int dfs(TreeNode root, long targetSum) {
+        if (root == null) {
+            return 0;
+        }
+        int ans = 0;
+        if (targetSum == root.val) {
+            ans += 1;
+        }
+
+        ans += dfs(root.left, targetSum - root.val);
+        ans += dfs(root.right, targetSum - root.val);
+        return ans;
+    }
+
+    @Test
+    public void testPathSum() {
+        Assert.assertEquals(3, pathSum(Utils.buildBinaryTree(new Integer[]{10, 5, -3, 3, 2, null, 11, 3, -2, null, 1}), 8));
+        Assert.assertEquals(3, pathSum(Utils.buildBinaryTree(new Integer[]{5, 4, 8, 11, null, 13, 4, 7, 2, null, null, 5, 1}), 22));
+        Assert.assertEquals(0, pathSum(Utils.buildBinaryTree(new Integer[]{1000000000, 1000000000, null, 294967296, null, 1000000000, null, 1000000000, null, 1000000000}), 0));
+    }
 }
