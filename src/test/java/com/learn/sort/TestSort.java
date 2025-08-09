d699608bcf7aaede59aca5303bfb6768cedc9fb2
haorui.herry <haorui.herry@dcarlife.com>
2025-08-09 16:43:11 +0800

    feat：binarySearch 增加测试case

--- src/test/java/com/learn/sort/TestSort.java
+++ src/test/java/com/learn/sort/TestSort.java
@@ -361,6 +361,11 @@ public class TestSort {
         int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9};
         for (int i = 0; i < 15; i++) {
             log.info("testBinarySearch i={} index={}", i, binarySearch(data, i));
+            if (i < 10) {
+                Assert.assertEquals(i - 1, binarySearch(data, i));
+            } else {
+                Assert.assertEquals(-1, binarySearch(data, i));
+            }
         }
     }
 
