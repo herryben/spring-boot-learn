fb681ba10a1d44552d1123c29fef820b903f049d
haorui.herry <haorui.herry@dcarlife.com>
2025-09-26 15:37:38 +0800

    feat: 80. 删除有序数组中的重复项 II

--- src/test/java/com/learn/array/ArraySolution.java
+++ src/test/java/com/learn/array/ArraySolution.java
@@ -300,4 +300,49 @@ public class ArraySolution {
         Assert.assertEquals(49, maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
         Assert.assertEquals(1, maxArea(new int[]{1, 1}));
     }
+
+    /**
+     * 80. 删除有序数组中的重复项 II
+     * 给你一个有序数组 nums ，请你 原地 删除重复出现的元素，使得出现次数超过两次的元素只出现两次 ，返回删除后数组的新长度。
+     * <p>
+     * 不要使用额外的数组空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
+     * 示例 1：
+     * <p>
+     * 输入：nums = [1,1,1,2,2,3]
+     * 输出：5, nums = [1,1,2,2,3]
+     * 解释：函数应返回新长度 length = 5, 并且原数组的前五个元素被修改为 1, 1, 2, 2, 3。 不需要考虑数组中超出新长度后面的元素。
+     * 示例 2：
+     * <p>
+     * 输入：nums = [0,0,1,1,1,1,2,3,3]
+     * 输出：7, nums = [0,0,1,1,2,3,3]
+     * 解释：函数应返回新长度 length = 7, 并且原数组的前七个元素被修改为 0, 0, 1, 1, 2, 3, 3。不需要考虑数组中超出新长度后面的元素。
+     *
+     * @param nums
+     * @return
+     */
+    public int removeDuplicates(int[] nums) {
+        if (nums.length < 2) {
+            return nums.length;
+        }
+        int ans = 1;
+        int left = 0, right = 1;
+        while (right < nums.length) {
+            if (nums[left] != nums[right]) {
+                ans++;
+                left = right;
+                right++;
+            } else {
+                if (right - left <) {
+                }
+                right++;
+            }
+        }
+        return ans;
+    }
+
+    @Test
+    public int testRemoveDuplicates() {
+        Assert.assertEquals(5, removeDuplicates(new int[]{1, 1, 2, 2, 3}));
+        Assert.assertEquals(7, removeDuplicates(new int[]{0, 0, 1, 1, 2, 3, 3}));
+    }
 }
