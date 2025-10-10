a5d541a0381f44f86149345b46d78b3ea092412e
haorui.herry <haorui.herry@dcarlife.com>
2025-10-10 18:18:04 +0800

    feat: feat: 543. 二叉树的直径

--- src/test/java/com/learn/dp/DepthFirstSearchSolution.java
+++ src/test/java/com/learn/dp/DepthFirstSearchSolution.java
@@ -15,37 +15,6 @@ import java.util.Queue;
  */
 public class DepthFirstSearchSolution {
     /**
-     * 207. 课程表
-     * https://leetcode.cn/problems/course-schedule/description/?envType=study-plan-v2&envId=top-100-liked
-     * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
-     * <p>
-     * 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
-     * <p>
-     * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
-     * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
-     * 示例 1：
-     * <p>
-     * 输入：numCourses = 2, prerequisites = [[1,0]]
-     * 输出：true
-     * 解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
-     * 示例 2：
-     * <p>
-     * 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
-     * 输出：false
-     * 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
-     * 解题思路：
-     * 1. 以出度为切入点进行dfs
-     * 1.1 最终不依赖其他课程的课程出度为0
-     *
-     * @param numCourses
-     * @param prerequisites
-     * @return
-     */
-    int[] visit;
-    List<List<Integer>> edges;
-    boolean valid = true;
-
-    /**
      * 200. 岛屿数量
      * https://leetcode.cn/problems/number-of-islands/
      * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
@@ -393,6 +362,37 @@ public class DepthFirstSearchSolution {
                 {0, 2},
         }));
     }
+
+    /**
+     * 207. 课程表
+     * https://leetcode.cn/problems/course-schedule/description/?envType=study-plan-v2&envId=top-100-liked
+     * 你这个学期必须选修 numCourses 门课程，记为 0 到 numCourses - 1 。
+     * <p>
+     * 在选修某些课程之前需要一些先修课程。 先修课程按数组 prerequisites 给出，其中 prerequisites[i] = [ai, bi] ，表示如果要学习课程 ai 则 必须 先学习课程  bi 。
+     * <p>
+     * 例如，先修课程对 [0, 1] 表示：想要学习课程 0 ，你需要先完成课程 1 。
+     * 请你判断是否可能完成所有课程的学习？如果可以，返回 true ；否则，返回 false 。
+     * 示例 1：
+     * <p>
+     * 输入：numCourses = 2, prerequisites = [[1,0]]
+     * 输出：true
+     * 解释：总共有 2 门课程。学习课程 1 之前，你需要完成课程 0 。这是可能的。
+     * 示例 2：
+     * <p>
+     * 输入：numCourses = 2, prerequisites = [[1,0],[0,1]]
+     * 输出：false
+     * 解释：总共有 2 门课程。学习课程 1 之前，你需要先完成​课程 0 ；并且学习课程 0 之前，你还应先完成课程 1 。这是不可能的。
+     * 解题思路：
+     * 1. 以出度为切入点进行dfs
+     * 1.1 最终不依赖其他课程的课程出度为0
+     *
+     * @param numCourses
+     * @param prerequisites
+     * @return
+     */
+    int[] visit;
+    List<List<Integer>> edges;
+    boolean valid = true;
 
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         visit = new int[numCourses];
